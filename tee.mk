08c2a3ff6e47270cd1d8ac8339f170c34f20a33e
ris <rissu.ntk@gmail.com>
2026-07-02 10:48:35 +0700

    a05m-tee: tee.mk: Remove Soong Namespace
    * Not needed.
    
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- tee.mk
+++ tee.mk
@@ -4,9 +4,6 @@
 
 TEE_DRIVER_PATH := vendor/samsung/a05m-tee
 
-PRODUCT_SOONG_NAMESPACES += \
-    $(TEE_DRIVER_PATH)
-
 # Init
 PRODUCT_COPY_FILES += \
     $(TEE_DRIVER_PATH)/prebuilts/tee_a05m.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/tee_a05m.rc
